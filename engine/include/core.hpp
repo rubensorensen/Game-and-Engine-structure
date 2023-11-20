6ebfd1072280daf8485b1578f3a99d8fe96f82fa
Ruben Sørensen <ruben.sorensen98@gmail.com>
2023-11-20 21:29:20 +0100

    Using recurse glob to collect files. Using API preprocessor macro to export/import

--- engine/include/core.hpp
+++ engine/include/core.hpp
@@ -1,3 +1,9 @@
 #pragma once
 
-__declspec(dllexport) int add_numbers(int a, int b);
+#ifdef COMPILING_ENGINE
+#define ENGINE_API __declspec(dllexport)
+#else
+#define ENGINE_API __declspec(dllimport)
+#endif
+
+ENGINE_API int add_numbers(int a, int b);
