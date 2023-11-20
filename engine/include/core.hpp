11317c103b170409426cd056a056f8862a932eb5
Ruben Sørensen <ruben.sorensen98@gmail.com>
2023-11-20 21:32:15 +0100

    Only specifying imports/exports if platform is Windows

--- engine/include/core.hpp
+++ engine/include/core.hpp
@@ -1,9 +1,17 @@
 #pragma once
 
+#ifdef _WIN32
+
 #ifdef COMPILING_ENGINE
 #define ENGINE_API __declspec(dllexport)
 #else
 #define ENGINE_API __declspec(dllimport)
+#endif
+
+#else
+
+#define ENGINE_API
+
 #endif
 
 ENGINE_API int add_numbers(int a, int b);
