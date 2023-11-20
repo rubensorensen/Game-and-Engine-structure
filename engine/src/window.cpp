c2a1a2aab9f60064e4732cb82db29e40a3cf0e23
Ruben Sørensen <ruben.sorensen98@gmail.com>
2023-11-20 22:53:00 +0100

    Update

--- engine/src/window.cpp
+++ engine/src/window.cpp
@@ -5,31 +5,40 @@
 static bool g_glfw_initted = false;
 static int g_window_count = 0; // Used to determine if GLFW should remain initialized, meaning if all windows are closed, GLFW will be terminated. May not be a perfect solution but it works.
 
-void glfw_error_callback(int error, const char* description)
+void
+glfw_error_callback(int error, const char* description)
 {
-	std::cerr << "GLFW error: " << description << std::endl;
+	std::cerr << "[ERROR] GLFW error: " << description << std::endl;
+}
+
+static void
+init_glfw()
+{    
+    if (!glfwInit()) {
+        throw std::runtime_error("GLFW could not be initialized");
+    }
+
+    glfwSetErrorCallback(glfw_error_callback);
+
+    g_glfw_initted = true;
+    g_window_count += 1;
 }
 
 Window::Window(const std::string& title, unsigned int width, unsigned int height)
 	: title(title), width(width), height(height)
 {
 	if (!g_glfw_initted) {
-		if (!glfwInit()) {
-			throw std::runtime_error("Ayo GLFW don't work");
-		}
-
-		glfwSetErrorCallback(glfw_error_callback);
-
-		g_glfw_initted = true;
-		g_window_count += 1;
+        init_glfw();
 	}
 
 	this->glfw_window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
 	if (!this->glfw_window) {
-		throw std::runtime_error("GLFW window creation failed.");
+		throw std::runtime_error("GLFW failed to create window.");
 	}
 
 	g_window_count += 1;
+
+    std::cout << "[INFO] Window created!" << std::endl;
 }
 
 Window::~Window()
