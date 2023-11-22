ead77793b54db3781e3341f55a9ebbe40c05d459
Ruben Sørensen <ruben.sorensen98@gmail.com>
2023-11-22 14:52:37 +0100

    Split engine and game into separate CMake projects

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -6,19 +6,20 @@
 
 int main(void)
 {
-    std::unique_ptr<Window> window;
-    try {
-        window = std::make_unique<Window>("Window 1", 1280, 720);
-    } catch (std::runtime_error e) {
-        std::cerr << "[ERROR] Could not create window: " << e.what() << std::endl;
-        return 0;
-    }
+	std::unique_ptr<Window> window;
+	try {
+		window = std::make_unique<Window>("Window 1", 1280, 720);
+	}
+	catch (std::runtime_error e) {
+		std::cerr << "[ERROR] Could not create window: " << e.what() << std::endl;
+		return 0;
+	}
 
-    while (!window->should_close()) {
-        window->update();
-        
-        poll_events();
-    }
+	while (!window->should_close()) {
+		window->update();
+
+		poll_events();
+	}
 
 	return 0;
 }
